3ff61563a595e84f0831b24c6de10ef7046d6374
Davyd McColl <davydm@gmail.com>
2016-06-12 21:02:26 +0200

    Rename module to ngScroll, like other similar modules

--- src/register.js
+++ src/register.js
@@ -2,6 +2,6 @@ const
   wheelUp = require('./ng-scroll-up.directive'),
   wheelDown = require('./ng-scroll-down.directive')
 
-const scrollModule = angular.module('ng-scroll', []);
+const scrollModule = angular.module('ngScroll', []);
 wheelUp(scrollModule);
 wheelDown(scrollModule);
